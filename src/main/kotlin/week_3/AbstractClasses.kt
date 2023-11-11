b4bbaa7d20d33e2a86e32578ebb98b4d8eed284a
patthhar <parthdroidworld@gmail.com>
2023-11-11 08:29:20 +0530

    Week 3 partial code added

---
+++ src/main/kotlin/week_3/AbstractClasses.kt
@@ -0,0 +1,41 @@
+package week_3
+
+abstract class BaseCourse {
+
+  val basicMaterial: Int = 0
+  fun courseMaterial(): List<String> {
+    return emptyList()
+  }
+
+  abstract fun material(): List<String>
+
+}
+
+abstract class AbstractCourse(
+  private val courseName: String
+) : BaseCourse() {
+  val stateHolder: Int = 1
+  abstract fun startingCourse(): String
+  fun startCourse(): String {
+    return "Course started"
+  }
+
+  override fun material(): List<String> {
+    println(basicMaterial)
+    println(courseName)
+    return courseMaterial()
+  }
+}
+
+class KmmCourse : AbstractCourse("Kmm") {
+  override fun startingCourse(): String {
+    println(stateHolder)
+    println(material())
+    return startCourse()
+  }
+}
+
+fun main() {
+  val kmmCourse = KmmCourse()
+  println(kmmCourse.startingCourse())
+}
